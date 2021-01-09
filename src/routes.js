0ef1d91b8c19db635e4bb5fcf66284d9c9124df6
lopes <lopes@arquia.com>
2021-01-09 14:55:43 -0300

    Primeiro commit - add base e material-ui

---
+++ src/routes.js
@@ -0,0 +1,18 @@
+import { Switch, Route } from 'react-router-dom'
+
+import Dashboard from './pages/Dashboard'
+import Detail from './pages/Detail'
+
+function Routes() {
+  return(
+
+      <Switch>
+        <Route component={Dashboard} path={'/'} exact />
+        <Route component={Dashboard} path={'/dashboard'}/>
+        <Route component={Detail} path={'/detail:id'}/>
+      </Switch>
+
+  )
+}
+
+export default Routes
